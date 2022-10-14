bc3aaae52b291e8ade733fecb209b74d20feffd5
toa.pita <toa.pita@gmail.com>
2022-10-14 12:44:16 -0600

    minor debuggin

--- dataStructuresProject.py
+++ dataStructuresProject.py
@@ -1,21 +1,9 @@
-# Dallin Duke
-# The Dictionary and remove queue
- 
-# Masons add to queue function
- 
 customerQueue = []
- 
 customerDictionary = {}
+from data_structures_project import Customer
 for x in range(100):
-    order = order()
- 
-    customer = Customer(order)
- 
+    customer = Customer()
     customerQueue.append(customer)
- 
 for x in range(0, len(customerQueue)) :
     cust = customerQueue.pop(0)
     customerDictionary[cust.customer_name] = cust.order.burger_count
-   
- 
-
--- mainPage.py
+++ mainPage.py
@@ -1,5 +1,19 @@
 from createOutput import createOutput
+from data_structures_project import Customer
+
+customerQueue = []
+customerDictionary = {}
+
+for x in range(100):
+    customer = Customer()
+    customerQueue.append(customer)
+for x in range(0, len(customerQueue)) :
+    cust = customerQueue.pop(0)
+    if cust.customer_name in customerDictionary:
+        customerDictionary[cust.customer_name] += cust.order.burger_count
+    else:
+        customerDictionary[cust.customer_name] = cust.order.burger_count
 # take each item from the dictionary and add it to a list sorted on values in descending order
 
-output = createOutput() #To-Do pass in whatever variable name is storing the dictionary of orders
+output = createOutput(customerDictionary)
 print(output)
